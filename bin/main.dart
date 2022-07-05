2da50dcd640303794dcae6423e5fa05b2a5eeaa7
saloh7856 <salohiddinberdiyorov5@gmail.com>
2022-07-05 13:47:46 +0500

    x

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,14 @@
 // babyloniam method
 // create function named func with arguments a and b
 // The variables are S and d given
-
-void main() {}
+double func(int s,d){
+    double a,b,x;
+    a=(s-d*d)/(2*d);
+    b=a+d;
+    x=b-a*a/(2*b);
+    return x;
+}
+void main(){
+    
+    print(func(16,3));
+}
